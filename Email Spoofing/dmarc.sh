00dd8099304eeb0006d5284f2e1e8e0e381ccbba
Muhammad Daffa <36522826+MD15@users.noreply.github.com>
2020-04-10 18:39:37 +0700

    Create dmarc.sh

---
+++ Email Spoofing/dmarc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+# DMARC Checker
+LISTS=$1
+
+if [[ ! -f ${LISTS} ]]; then
+	echo "usage: bash $0 list.txt"
+	exit
+fi
+
+for SITE in $(cat $LISTS);
+do
+    if [[ $(curl --connect-timeout 3 -kls --max-time 3 -d "action=dm_integration_inspect_dmarc&domain=${SITE}&security=83b182c4ce" -X POST "https://dmarcian.com/wp-admin/admin-ajax.php") =~ 'Congratulations! Your DMARC record is valid.' ]]; then
+		echo -e "\e[32m[+] VULN: ${SITE}"
+	else
+		echo -e "\e[31m[-] NOT VULN: ${SITE}"
+	fi
+    
+done
