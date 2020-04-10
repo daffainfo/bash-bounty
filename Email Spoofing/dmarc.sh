221916603816e5ba849640e1070d26b41bcd139d
Muhammad Daffa <36522826+MD15@users.noreply.github.com>
2020-04-10 18:52:33 +0700

    Update dmarc.sh

--- Email Spoofing/dmarc.sh
+++ Email Spoofing/dmarc.sh
@@ -9,7 +9,7 @@ fi
 
 for SITE in $(cat $LISTS);
 do
-    if [[ $(curl --connect-timeout 3 -kls --max-time 3 -d "action=dm_integration_inspect_dmarc&domain=${SITE}&security=83b182c4ce" -X POST "https://dmarcian.com/wp-admin/admin-ajax.php") =~ 'Congratulations! Your DMARC record is valid.' ]]; then
+    if [[ $(curl --connect-timeout 3 -kls --max-time 3 -d "action=dm_integration_inspect_dmarc&domain=${SITE}&security=83b182c4ce" -X POST "https://dmarcian.com/wp-admin/admin-ajax.php") =~ 'No DMARC record published.' ]]; then
 		echo -e "\e[32m[+] VULN: ${SITE}"
 	else
 		echo -e "\e[31m[-] NOT VULN: ${SITE}"
