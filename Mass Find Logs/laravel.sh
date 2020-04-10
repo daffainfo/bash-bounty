31866b9b29a78bd38d7d7f6ef2bfaab1388b3693
Muhammad Daffa <36522826+MD15@users.noreply.github.com>
2020-04-10 18:27:11 +0700

    Create laravel.sh

---
+++ Mass Find Logs/laravel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Log Laravel Mass Scanner 
+LISTS=$1
+
+if [[ ! -f ${LISTS} ]]; then
+	echo "ERROR: ${LISTS} not found"
+	echo "usage: bash $0 list.txt"
+	exit
+fi
+
+for SITE in $(cat $LISTS);
+do
+    if [[ $(curl --connect-timeout 3 --max-time 3 -kLs "${SITE}/wp-content/debug.log" ) =~ 'laravel\framework' ]]; then
+		echo -e "\e[32m[+] FOUND: ${SITE}storage/logs/laravel.log"
+	else
+		echo -e "\e[31m[-] NOT FOUND: ${SITE}"
+	fi
+    
+done
