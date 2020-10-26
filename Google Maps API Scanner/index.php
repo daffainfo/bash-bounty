0a22f7c9af4fcd6c401fca2f907ffc39ea2f0fe1
Muhammad Daffa <36522826+daffainfo@users.noreply.github.com>
2020-10-26 22:09:40 +0700

    Create index.php

---
+++ Google Maps API Scanner/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Google Maps API Key Checker</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center my-5">Input Google Maps API Key</h1>
+        <form action="result.php" method="post" align="center" class="my-5">
+            <div class="form-group">
+                <label for="apikey">Input API </label>
+                <input class="form-control" name="apikey">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</body>
+</html>
